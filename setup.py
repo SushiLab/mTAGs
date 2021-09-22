a1496adddaf11e14362a8566a1950aa64d49f621
Hans <hansr@ethz.ch>
2021-09-22 11:25:32 +0200

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "mTAGs",
-    version = "1.0.2",
+    version = "1.0.3",
     author = "Hans-Joachim Ruscheweyh",
     author_email = "hansr@ethz.ch",
     description = ("mTAGs - taxonomic profiling using degenerate consensus reference sequences of ribosomal RNA gene."),
@@ -11,7 +11,7 @@ setup(
     keywords = "bioinformatics metagenomics ngs 16S",
     url = "https://github.com/SushiLab/mTAGs",
     packages=['mTAGs'],
-    download_url = "https://github.com/SushiLab/mTAGs/archive/refs/tags/1.0.2.tar.gz",
+    download_url = "https://github.com/SushiLab/mTAGs/archive/refs/tags/1.0.3.tar.gz",
     entry_points = {
         'console_scripts': ['mtags=mTAGs.mtags:main'],
     }
